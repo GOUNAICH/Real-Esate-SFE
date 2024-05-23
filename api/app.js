4e6dc5630221727fdd4931296d0801ee8aec1ec3
GOUNAICH <abdeslamgounaich61@gmail.com>
2024-05-23 19:08:53 +0200

    HHHH

--- api/app.js
+++ api/app.js
@@ -1,18 +1,17 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import authRoute from "./routes/auth.route.js";
-import postRoute from "./routes/post.route.js";
-import testRoute from "./routes/test.route.js";
-import userRoute from "./routes/user.route.js";
-import chatRoute from "./routes/chat.route.js";
-import messageRoute from "./routes/message.route.js";
-import commentRoute from "./routes/comment.route.js";
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authRoute from './routes/auth.route.js';
+import postRoute from './routes/post.route.js';
+import testRoute from './routes/test.route.js';
+import userRoute from './routes/user.route.js';
+import chatRoute from './routes/chat.route.js';
+import messageRoute from './routes/message.route.js';
+import commentRoute from './routes/comment.route.js';
 
 const app = express();
 
-// Use environment variable for the client URL
-const clientUrl = process.env.CLIENT_URL || "http://localhost:5173";
+const clientUrl = process.env.CLIENT_URL || 'http://localhost:5173';
 
 app.use(cors({
   origin: clientUrl,
@@ -22,15 +21,19 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/test", testRoute);
-app.use("/api/chats", chatRoute);
-app.use("/api/messages", messageRoute);
-app.use("/api/comments", commentRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
+app.use('/api/test', testRoute);
+app.use('/api/chats', chatRoute);
+app.use('/api/messages', messageRoute);
+app.use('/api/comments', commentRoute);
 
-
+// Log all incoming requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 const port = process.env.PORT || 8800;
 app.listen(port, () => {
--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -1,14 +1,21 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
+  
+  if (!token) {
+    console.log("Token not found in cookies");
+    return res.status(401).json({ message: "Not Authenticated!" });
+  }
 
-  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, payload) => {
+    if (err) {
+      console.log("Token verification failed", err);
+      return res.status(403).json({ message: "Token is not Valid!" });
+    }
 
-  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) return res.status(403).json({ message: "Token is not Valid!" });
     req.userId = payload.id;
-
     next();
   });
 };
+
