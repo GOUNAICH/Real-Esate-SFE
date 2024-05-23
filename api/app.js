13540253bb5bba88f6d22e9544e5beef38ef7f76
GOUNAICH <abdeslamgounaich61@gmail.com>
2024-05-23 19:20:19 +0200

    changes

--- api/app.js
+++ api/app.js
@@ -1,41 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import authRoute from './routes/auth.route.js';
-import postRoute from './routes/post.route.js';
-import testRoute from './routes/test.route.js';
-import userRoute from './routes/user.route.js';
-import chatRoute from './routes/chat.route.js';
-import messageRoute from './routes/message.route.js';
-import commentRoute from './routes/comment.route.js';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
+import commentRoute from "./routes/comment.route.js";
 
 const app = express();
 
-const clientUrl = process.env.CLIENT_URL || 'http://localhost:5173';
-
-app.use(cors({
-  origin: clientUrl,
-  credentials: true,
-}));
-
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/auth', authRoute);
-app.use('/api/users', userRoute);
-app.use('/api/posts', postRoute);
-app.use('/api/test', testRoute);
-app.use('/api/chats', chatRoute);
-app.use('/api/messages', messageRoute);
-app.use('/api/comments', commentRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/comments", commentRoute);
 
-// Log all incoming requests
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-const port = process.env.PORT || 8800;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}!`);
+app.listen(8800, () => {
+  console.log("Server is running!");
 });
