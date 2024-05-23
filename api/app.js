5fa76f635e78dca38f64b9ba9aae9897f9ad36fd
GOUNAICH <abdeslamgounaich61@gmail.com>
2024-05-23 04:10:58 +0200

    app.js api

--- api/app.js
+++ api/app.js
@@ -11,7 +11,7 @@ import commentRoute from "./routes/comment.route.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
