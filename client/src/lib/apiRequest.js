a218e5b51c538cc2d16854d6dde1494e7c6a57fc
GOUNAICH <abdeslamgounaich61@gmail.com>
2024-05-24 17:58:10 +0200

    change baseURL

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://real-esate-sfe-backend.vercel.app",
   withCredentials: true,
 });
 
